9fc0ef4dcbb41d002ad344735aeaf5b95166bd24
Amargabr55 <93977190+Amargabr55@users.noreply.github.com>
2024-03-19 18:28:52 -0700

    Update app.py

--- app.py
+++ app.py
@@ -21,47 +21,6 @@ def predict():
                   int(data['Survival Months']), int(data['breast_cancer_history'])]
     prediction = model.predict([input_data])
     predicted_stage = label_encoder.inverse_transform(prediction)
-    return render_template('index.html', prediction_text="Predicted stage: {}".format(predicted_stage))
+    return jsonify({'prediction_text': "Predicted stage: {}".format(predicted_stage)})
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
-"""
-from flask import Flask, request, jsonify
-import pickle
-import numpy as np
-
-# Load the trained model
-with open('breast_cancer.pkl', 'rb') as model_file:
-    model = pickle.load(model_file)
-import requests   
-import json 
-
-app = Flask(__name__)
-
-url = 'http://127.0.0.1:5000/predict'
-# Define endpoint for making predictions
-@app.route('/predict', methods=['POST'])
-def predict():
-    data = request.get_json(force=True)
-    # Assuming the incoming data is a list of features
-    features = data['features']
-    # Convert features to numpy array and make prediction
-    features_array = np.array(features).reshape(1, -1)
-    prediction = model.predict(features_array)
-    # Convert prediction to a JSON serializable format (Python list)
-    prediction_list = prediction.tolist()
-    return jsonify({'prediction': prediction_list})
-    json_data = json.dumps(data)
-    response = requests.post(url, json=json_data)
-    
-    if response.status_code == 200:
-        prediction = response.json()['prediction']
-        print("Prediction:", prediction)
-    else:
-        print("Error:", response.status_code)
-        
-
-
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
-    """
