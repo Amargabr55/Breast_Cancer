7d8b0f1f409f10729b4a94a72748d5629d38f494
Amargabr55 <93977190+Amargabr55@users.noreply.github.com>
2024-03-21 17:13:40 -0700

    Update app.py

--- app.py
+++ app.py
@@ -15,13 +15,25 @@ def home():
 @app.route('/predict', methods=['POST'])
 def predict():
     if request.method == 'POST':
-        # Check if the request is properly formatted as JSON
-        if not request.json:
-            return jsonify({'error': 'Request must be in JSON format'}), 400
-        
-        # Get the JSON data from the request
-        data = request.json
-        
+        # Simulated JSON data for prediction
+        data = {
+            'Race': 'Caucasian',
+            'Marital Status': 'Married',
+            'N Stage': 1,
+            '6th Stage': 2,
+            'Differentiate': 'Well',
+            'Grade': 'I',
+            'A Stage': 'T1',
+            'Estrogen Status': 'Positive',
+            'Progesterone Status': 'Negative',
+            'Age': 45,
+            'Tumor_Size': 2.5,
+            'Regional Node Examined': 3,
+            'Reginol Node Positive': 0,
+            'Survival Months': 60,
+            'Breast Cancer History': 'No'
+        }
+
         # Check if all required fields are present
         required_fields = [
             'Race', 'Marital Status', 'N Stage', '6th Stage',
